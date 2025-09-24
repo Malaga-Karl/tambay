826def5212aeb70c9f89d9ca9a2cac7676303981
Karl Malaga <malaga.karlbenedict@gmail.com>
2025-09-24 21:36:39 +0800

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Tambay',
       theme: ThemeData(
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
